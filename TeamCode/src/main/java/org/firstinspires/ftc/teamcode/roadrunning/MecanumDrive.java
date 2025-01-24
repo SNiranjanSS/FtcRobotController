e177541ecff40e38001d57f3fac12c5613bf18a2
SNiranjanSS <saisainiranjan@gmail.com>
2025-01-24 11:52:28 -0600

    Initial

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunning/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunning/MecanumDrive.java
@@ -239,7 +239,7 @@ public final class MecanumDrive {
 
         // TODO: make sure your config has an IMU with this name (can be BNO or BHI)
         //   see https://ftc-docs.firstinspires.org/en/latest/hardware_and_software_configuration/configuring/index.html
-        lazyImu = new LazyImu(hardwareMap, "imu", new RevHubOrientationOnRobot(
+        lazyImu = new LazyImu(hardwareMap, "BHI260AP", new RevHubOrientationOnRobot(
                 PARAMS.logoFacingDirection, PARAMS.usbFacingDirection));
 
         voltageSensor = hardwareMap.voltageSensor.iterator().next();
